c1c2597accf565dddd6e176e2ad084f778895f48
Mauro Gonzalez <maurogonzalezbruno@gmail.com>
2020-02-20 19:36:49 -0300

    corregida las fechas

--- reporte.sh
+++ reporte.sh
@@ -6,35 +6,35 @@ user=$2
 pw=$3
 db=$4
 
-fecha=`date -d yesterday +%Y-%m-%d`
+fechaI=`date -d yesterday +%Y-%m-%d`
+fechaF=`date +%Y-%m-%d`
 
-sql="select count(*) from vicidial_list where list_id='1999' and entry_date like '$fecha%';"
+sql="select count(*) from vicidial_list where list_id='1999' and entry_date >= date_add('$fechaI',interval 5 hour) and 
+	entry_date < date_add('$fechaF',interval 5 hour);"
 
 sql2="select count(*) from vicidial_list where list_id='1999' 
-	and modify_date like '$fecha%' and ( status='SDFP' or status='SDPD' or status='SDPP' or status='SPFP' 
+	and modify_date >= '$fechaI' and modify_date < '$fechaF' and ( status='SDFP' or status='SDPD' or status='SDPP' or status='SPFP' 
 	or status='SPPD' or status='SPPP' );"	
 
-sql3="select count(*) from vicidial_list where list_id='1999' 
-	and modify_date like '$fecha%' and ( status='AC' or status='AD' or status='CALLBK' or status='DNC' 
-	or status='HU' or status='NI' or status='SALE' or status='WSUO' or status='WN' );"	
+sql3="select count(*) from vicidial_list where list_id='1999' and ( modify_date >= '$fechaI%' and modify_date < '$fechaF' 
+	and status IN ( 'AC','AD','CALLBK','DNC','HU','NI','SALE','WSUO','WN','INT','AR' ));"	
 
 mysql --host=$host -u $user -p$pw -D$db -e "$sql" > /tmp/leads_day.log
 mysql --host=$host -u $user -p$pw -D$db -e "$sql2" > /tmp/leads_sales.log
 mysql --host=$host -u $user -p$pw -D$db -e "$sql3" > /tmp/leads_contacts.log
 
-
 leads_day=`cat /tmp/leads_day.log | sed -n '2 p'`
 leads_sales=`cat /tmp/leads_sales.log | sed -n '2 p'`
 leads_contacts=`cat /tmp/leads_contacts.log | sed -n '2 p'`
 
-let leads_vs_sales2=$leads_sales*100/$leads_day
+#let leads_vs_sales2=$leads_sales*100/$leads_day
 leads_vs_sales=$( echo "scale=2; $leads_sales*100/$leads_day" | bc )
 contacts_vs_sales=$( echo "scale=2; $leads_sales*100/$leads_contacts" | bc )
 
 
-echo "Cantidad de leads ingresados el dia $fecha = $leads_day"
-echo "Cantidad de Venta en el dia $fecha = $leads_sales"
-echo "Cantidad de Contactos en el dia $fecha = $leads_contacts"
+echo "Cantidad de leads ingresados el dia $fechaI = $leads_day"
+echo "Cantidad de Venta en el dia $fechaI = $leads_sales"
+echo "Cantidad de Contactos en el dia $fechaI = $leads_contacts"
 echo "Leads ingresados por dia VS Sales = $leads_vs_sales"
 echo "Leads Contacts VS Sales = $contacts_vs_sales"
 
