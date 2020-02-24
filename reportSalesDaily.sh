fa0436c977b2a0546b4f6166402cc7c1f9359e51
Mauro Gonzalez <maurogonzalezbruno@gmail.com>
2020-02-24 11:45:02 -0300

    reporte terminado

--- reportSalesDaily.sh
+++ reportSalesDaily.sh
@@ -15,15 +15,14 @@ parametros
 
 FECHAF=`date +%Y-%m-%d`
 FECHAI=`date +%Y-%m-%d -d "$FECHAF -1 day"`
-IP="192.168.70.13"
-PW="internalreports"
-NAME_OD="OnlineDivorce-report-Sales"
-CID_OD="ODIVORCE"
-MAIL_OD="mgonzalez@telecomnetworks.net"
-STATUS_OD_S="'SU','SDFP','SDPD','SDPP','SPFP','SPPD','SPPP'"	
-STATUS_OD_C="'AC','AD','CALLBK','DNC','HU','NI','SALE','WSUO','WN','INT','AR'"
-INTERVAL_OD="5"
+IP="192.168.1.250"
+PW="secret"
+NAME="Example-report-Sales"
+CID="EXAMPLE"
+MAIL="example@example.com"
+STATUS_S="'SU','SDFP','SDPD','SDPP','SPFP','SPPD','SPPP'"	
+STATUS_C="'AC','AD','CALLBK','DNC','HU','NI','SALE','WSUO','WN','INT','AR'"
+INTERVAL="5"
 
-/bin/bash "/var/opt/cron/vicibox/reportSales.sh" "$IP" "$PW" "$CID_OD" "$MAIL_OD" "$STATUS_OD_S" "$STATUS_OD_C" "$NAME_OD" "$FECHAI" "$FECHAF" "$INTERVAL_OD"
-#/bin/bash "/home/mgonzalez/Documents/Telecom/git/reportSales/reportSales.sh" "$IP" "$PW" "$CID_OD" "$MAIL_OD" "$STATUS_OD_S" "$STATUS_OD_C" "$NAME_OD" "$FECHAI" "$FECHAF" "$INTERVAL_OD"
-
+/bin/bash "/var/opt/cron/reportSales.sh" "$IP" "$PW" "$CID" "$MAIL" "$STATUS_S" "$STATUS_C" "$NAME" "$FECHAI" "$FECHAF" "$INTERVAL"
+#/bin/bash "/home/mgonzalez/Documents/Telecom/git/reportSales/reportSales.sh" "$IP" "$PW" "$CID" "$MAIL" "$STATUS_S" "$STATUS_C" "$NAME" "$FECHAI" "$FECHAF" "$INTERVAL"
